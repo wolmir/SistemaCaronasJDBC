6e864d4ab579a9a54dda1d94c63001a461b9ae1c
Usuario <Usuario@Usuario-PC>
2013-01-23 20:14:21 -0200

    Veiculos Crud 100%

--- SistemaCaronasJDBC/src/java/com/model/dao/TipoVeiculoDAO.java
+++ SistemaCaronasJDBC/src/java/com/model/dao/TipoVeiculoDAO.java
@@ -11,12 +11,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Usuario
  */
-class TipoVeiculoDAO {
+public class TipoVeiculoDAO {
 private Connection connection;
     
     public TipoVeiculoDAO() {
@@ -45,6 +47,24 @@ private Connection connection;
             e.printStackTrace();
         }
         return null;
+    }
+    
+    public List<TipoVeiculo> getTiposVeiculos() {
+        String sql = "select * from tipo_veiculo";
+        List<TipoVeiculo> tipos = new ArrayList<TipoVeiculo>();
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                TipoVeiculo tipo = new TipoVeiculo();
+                tipo.setId(rs.getInt("id_tipo_veiculo"));
+                tipo.setTipoVeiculo(rs.getString("tipo_veiculo"));
+                tipos.add(tipo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tipos;
     }
     
 }
